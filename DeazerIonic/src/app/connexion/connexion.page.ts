dc9ba8b6fbe31a18a26a4ec9d07465d93ab119da
BALDASSERONI <sebastien.baldasseroni@gmail.com>
2022-04-28 21:04:08 -0300

    ionic deazer

---
+++ DeazerIonic/src/app/connexion/connexion.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.page.html',
+  styleUrls: ['./connexion.page.scss'],
+})
+export class ConnexionPage implements OnInit {
+  email
+  password
+  token
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  connexion() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "identifier": this.email,
+      "password": this.password
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+
+    };
+
+    fetch("http://localhost:1337/api/auth/local", requestOptions)
+      .then(response => response.json())
+      .then((result) => {
+        this.token = result
+
+        if (this.token.jwt) {
+          this.navigate()
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+  navigate() {
+    this.router.navigate(['/tabs/tab1'])
+  }
+
+}
